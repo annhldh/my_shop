1c070ec1a34cad9a7cd58304084f9fe4b9bf6704
annhldh <23020004@vnu.edu.vn>
2024-08-24 00:15:35 +0700

    f

--- public/app.js
+++ public/app.js
@@ -1,26 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhs3M24W62T09sviVORtNoYWmMLo6Fo54",
+  authDomain: "shop-9a5be.firebaseapp.com",
+  projectId: "shop-9a5be",
+  storageBucket: "shop-9a5be.appspot.com",
+  messagingSenderId: "817465241049",
+  appId: "1:817465241049:web:ef976d9ccf1e21431d3dc8",
+  measurementId: "G-DLVYXMMBLC"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAhs3M24W62T09sviVORtNoYWmMLo6Fo54",
-    authDomain: "shop-9a5be.firebaseapp.com",
-    projectId: "shop-9a5be",
-    storageBucket: "shop-9a5be.appspot.com",
-    messagingSenderId: "817465241049",
-    appId: "1:817465241049:web:ef976d9ccf1e21431d3dc8",
-    measurementId: "G-DLVYXMMBLC"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Hàm thêm dữ liệu vào Firestore
